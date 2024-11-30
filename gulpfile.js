aa1c169167e73b997c771c5e91f42bde31074289
Lucas <lucas.flixzin@gmail.com>
2024-11-29 22:13:44 -0300

    criando o hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-// const imagemin = require('gulp-imagemin');
+const imagemin = require('gulp-imagemin');
 // const uglify = require('gulp-uglify');
 
 // function scripts(){
@@ -16,16 +16,14 @@ function styles() {
     .pipe(gulp.dest('./dist/css'));
 }
 
-// function images() {
-//     return gulp.src('./src/images/**/*')
-//     .pipe(imagemin())
-//     .pipe(gulp.dest('./dist/images'));
-// }
+function images() {
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
 
-// exports.default = gulp.parallel(styles, images, scripts);
-// exports.watch = function() {
-//     gulp.watch ('./src/styles/*.scss', gulp.parallel(styles))
+exports.default = gulp.parallel(styles, images);
+exports.watch = function() {
+    gulp.watch ('./src/styles/*.scss', gulp.parallel(styles))
 //     gulp.watch ('./src/styles/*.scss', gulp.parallel(scripts))
-// }
-
-exports.default = styles;
+}
